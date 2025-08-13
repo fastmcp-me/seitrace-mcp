afc7ffae11fadc7aa6a97c7b3a7f451ebb602148
0xtrou <109982124+0xtrou@users.noreply.github.com>
2025-08-13 20:28:35 +0700

    chore: prepare for snippet action

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,14 @@
 import { jsonSchemaToZod } from 'json-schema-to-zod';
 import { z } from 'zod';
+import oasToSnippet from '@readme/oas-to-snippet';
+import Oas from 'oas';
+import fs from 'fs';
+import NodePath from 'path';
 
 import { endpointDefinitionMap } from './openapi-definition.js';
 import { McpGroupedToolDefinition, McpToolDefinition } from './types.js';
+import { Language } from '@readme/oas-to-snippet/languages';
+import { fileURLToPath } from 'url';
 
 /**
  * Utility: convert CamelCase or mixedCase to snake_case
@@ -129,3 +135,63 @@ export const groupedToolDefinitionMap: Map<string, McpGroupedToolDefinition> = (
 
   return map;
 })();
+
+/**
+ * The function to generate code snippets from the API specifications
+ * @param path The API endpoint path
+ * @param language The programming language for the snippet
+ * @example
+ * > generateSnippet('/api/v2/token/erc20/transfers', 'node')
+  "const url = 'https://seitrace.com/insights/api/v2/token/erc20/transfers?limit=10&offset=0&chain_id=pacific-1&contract_address=0x0c78d371EB4F8c082E8CD23c2Fa321b915E1BBfA&wallet_address=0x0c78d371EB4F8c082E8CD23c2Fa321b915E1BBfA&from_date=2021-01-01&to_date=2021-03-01';\n" +
+  'const options = {\n' +
+  "  method: 'GET',\n" +
+  "  headers: {accept: 'application/json', 'x-api-key': 'luclf6g1sbc'}\n" +
+  '};\n' +
+  '\n' +
+  'fetch(url, options)\n' +
+  '  .then(res => res.json())\n' +
+  '  .then(json => console.log(json))\n' +
+  '  .catch(err => console.error(err));'
+ */
+export const generateSnippet = (path: string, language: string) => {
+  // Prepare openapi specs
+  const fileName = fileURLToPath(import.meta.url);
+  const dirName = NodePath.dirname(fileName);
+  const specs = fs.readFileSync(NodePath.join(dirName, './api-specs.json')).toString();
+  const apiDefinition = new Oas(JSON.parse(specs as any));
+  const operation = apiDefinition.operation(path, 'get'); // all are get methods
+
+  const formData = {
+    // Construct examples data
+    query: operation.getParameters().reduce((accum, param) => {
+      if (param.in === 'query') {
+        accum[param.name] = param.example;
+      }
+      return accum;
+    }, {} as Record<string, any>),
+
+    // Construct examples data
+    header: {
+      ...operation.getHeaders().request.reduce((accum, header) => {
+        if (header.toLowerCase() === 'accept' || header.toLowerCase() === 'content-type') {
+          accum[header] = 'application/json';
+        } else {
+          accum[header] = Math.random().toString(36).substring(2, 15);
+        } // Example value
+        return accum;
+      }, {} as Record<string, any>),
+      // Should additionally include content-type header
+      'content-type': 'application/json',
+    },
+  };
+
+  // Prepare security headers
+  const auth = Object.keys(operation.schema.security?.[0] || {}).reduce((accum, key) => {
+    accum[key] = Math.random().toString(36).substring(2, 15); // Example value
+    return accum;
+  }, {} as Record<string, any>);
+
+  // Return the generated code snippet
+  const { code } = oasToSnippet(apiDefinition, operation, formData, auth, language as Language);
+  return code;
+};
